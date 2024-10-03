621d5759e373140576009287fd8fc7566e58d02e
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-10-03 05:59:08 +0530

    delete skill to update resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,7 +2,7 @@ const Resume = () => {
     return (
       <>
         <div
-          id="/skill"
+          id="/resume"
           className=" text-3xl text-red-700 font-semibold m-3 border-4 border-green-500 flex flex-wrap  justify-center items-center min-h-screen"
           >
             <p>Resume is under devlopment </p>
