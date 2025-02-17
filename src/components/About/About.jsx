c91348ec1aea7712f7d4872e7d98cd20ded9f814
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2025-02-17 13:55:35 +0530

    `Removed unused import and adjusted layout in About component`

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 // import VideoSource from "../../assets/video/AboutVideo.mp4";
-import AboutImg from "../../assets/AboutPic.jpg";
+// import AboutImg from "../../assets/AboutPic.jpg";
 // import { motion } from "framer-motion";
 import Card from "../Card/card";
 
@@ -11,16 +11,16 @@ const About = () => {
         id="/about"
         className="m-3 flex flex-wrap justify-center h-auto md:max-h-screen"
       >
-        <div className="flex flex-wrap justify-between m-1 w-full md:w-[80%]  ">
+        <div className="flex flex-wrap flex-col-reverse sm:flex-row  justify-between m-1 w-full md:w-[80%]  ">
 
-          <div className=" md:hidden flex flex-wrap justify-end  items-end  overflow-hidden rounded-md md:w-[40%] ml-5 mt-10 mb-3">
+          {/* <div className=" md:hidden flex flex-wrap justify-end  items-end  overflow-hidden rounded-md md:w-[40%] ml-5 mt-10 mb-3">
             <img
               src={AboutImg}
               alt="About Image"
               className=" h-[15rem] w-[23rem] rounded-lg border-2 shadow-md ml-3 "
             />
-          </div>
-          <div className="flex  flex-wrap w-full md:w-1/2  p-3">
+          </div> */}
+          <div className="flex  flex-wrap w-full md:w-1/2  p-3 mt-3">
             <h2 className="flex items-center w-full mx-3 mt-3 text-lg font-bold text-blue-600  ">
               ABOUT ME
             </h2>
@@ -44,8 +44,8 @@ const About = () => {
             </h1>
           </div>
      
-          <div className="flex flex-wrap justify-center  items-center  rounded-md md:w-[40%] ml-5">
-        <div>
+          <div className="flex flex-wrap justify-center  items-center  rounded-md md:w-[40%] mx-auto mb-10 ">
+       
           <Card/>
             {/* <video
               className=" hidden md:flex h-[20rem] w-[24rem] object-cover rounded-lg border-2 shadow-md ml-3 "
@@ -56,7 +56,7 @@ const About = () => {
               <source src={VideoSource} type="video/mp4" />
               Your browser does not support the video tag.
             </video> */}
-        </div>
+       
           </div>
         </div>
       </div>
